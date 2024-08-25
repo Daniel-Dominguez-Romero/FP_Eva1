8cc0d16e7b5f66cc6b29cb0219884c3d2f73c56b
Daniel-Dominguez-Romero <L24550320@chihuahua2.tecnm.mx>
2024-08-24 21:22:54 -0600

    Add files via upload

---
+++ FP_Eva_1/src/main/java/com/mycompany/fp_eva_1/FP_Eva_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fp_eva_1;
+
+/**
+ *
+ * @author danie
+ */
+public class FP_Eva_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+
+    }
+    
+    }
+    
