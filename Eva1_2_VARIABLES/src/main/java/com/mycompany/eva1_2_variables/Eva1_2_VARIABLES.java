e90d2c599f87fd96a66cafc84eea0b18253ed332
Daniel-Dominguez-Romero <L24550320@chihuahua2.tecnm.mx>
2024-08-26 17:37:20 -0600

    Add files via upload

---
+++ Eva1_2_VARIABLES/src/main/java/com/mycompany/eva1_2_variables/Eva1_2_VARIABLES.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables;
+
+/**
+ *
+ * @author daniel
+ */
+public class Eva1_2_VARIABLES {
+
+    public static void main(String[] args) {
+                                            //TIPOS DE DATOS
+        System.out.println("Hola Mundo!!");//Cadena de texto
+        System.out.println(100);//Entero
+        System.out.println("3.1416");//Número con punto flotante
+        System.out.println('c');//Caracter
+        System.out.println(true);//Valores lógicos (booleanos)
+        System.out.println(false  //Valores lógicos (booleanos)
+          
+                
+        )
+                ;//;------>FIN DE INSTRUCCIÓN (NO OLVIDAR)
+        //ENTEROS:
+        //int
+        //long
+        //shot
+        
+        //NUMEROS CON PARTE DECIMAL (FLOTANTES):
+        //flout
+        //double
+        
+        //CADENAS DE TEXTO: 
+        //String
+        
+        //CARACTERES:
+        //char
+        
+        //VALORES LOGICOS:
+        //boolean 
+        
+        //DECLARACION DE VARIABLE:
+        //SINTAXIS PARA DECLARAR VARIABLES (REGLAS) 
+        //TIPO_DE_DATO NOMBRE_DE_LA_VARIABLE;
+        String nombre;
+        String apellido; 
+        String curp;
+        double promedio;
+        int edad;
+        
+        edad = 18; //ASIGNACION: Estoy colocando el valor 18 
+                   //dentro de la variable edad 
+        nombre = "DANIEL";
+        apellido = "DOMINGUEZ DOMINGUEZ";
+        promedio = 9.0; 
+        
+        System.out.println(edad);
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(promedio);
+        
+    }
+}
