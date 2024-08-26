2285b57d2c461e5d14e613f63d495f8f34153b9a
Daniel-Dominguez-Romero <L24550320@chihuahua2.tecnm.mx>
2024-08-26 17:51:54 -0600

    Add files via upload

---
+++ Eva1_3_VARIABLES2/src/main/java/com/mycompany/eva1_3_variables2/Eva1_3_VARIABLES2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_variables2;
+
+/**
+ *
+ * @author danie
+ */
+public class Eva1_3_VARIABLES2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+       //VEHICULOS:
+       String marca; 
+       String modelo; 
+       int year; //la ñ en otros lenguajes no es aceptada 
+       double precio;
+       
+       //ASIGNACION
+       marca = "NISSAN";
+       modelo = "GTR";
+       year = 2008;
+       precio = 1000000.1;
+       
+       System.out.println(marca);
+       System.out.println(modelo);
+       System.out.println(year);
+       System.out.println(precio);
+       
+       
+    }
+}
